901461ab450f46c207b1680edd18f89f75f49a34
김승환 사원 ERP솔루션기술팀 (ksh0313@lgcns.com <82146@CORP.LGCNS.COM>
2020-05-16 17:52:38 +0900

    test controller

---
+++ src/main/java/me/seung/miniproject_seung/testController.java
@@ -0,0 +1,20 @@
+package me.seung.miniproject_seung;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class testController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String startPage() {
+        return "connection complete";
+    }
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "hello";
+    }
+}
