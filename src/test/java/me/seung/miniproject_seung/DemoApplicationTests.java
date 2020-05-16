4851e15b2a67e47fe6fd22c9a9a56a3ccb6cf0bd
김승환 사원 ERP솔루션기술팀 (ksh0313@lgcns.com <82146@CORP.LGCNS.COM>
2020-05-16 17:02:53 +0900

    init_0516

---
+++ src/test/java/me/seung/miniproject_seung/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.seung.miniproject_seung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
